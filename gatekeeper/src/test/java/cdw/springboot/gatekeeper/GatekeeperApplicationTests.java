074f47f077f6d632356bb0120a13b96071e28c80
Ranjith Saravanan <125631729+ranjithks-cdw@users.noreply.github.com>
2023-10-27 09:47:50 +0530

    init commit

---
+++ gatekeeper/src/test/java/cdw/springboot/gatekeeper/GatekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package cdw.springboot.gatekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
